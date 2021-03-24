fb3d596879098dcfb4f733a8d3db7d1f9839528e
marciobbs@gmail.com <marciobbs@gmail.com>
2021-03-24 23:57:04 +0000

    Create FavoriteMovies.js

---
+++ src/FavoriteMovie.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class FavoriteMovie extends Component {
+	render() {
+      	console.log(this.props.profiles);
+      	console.log(this.props.users);
+      	console.log(this.props.movies);
+    	return (
+        	<span></span>
+        )
+    }
+}
+
+export default FavoriteMovie;
